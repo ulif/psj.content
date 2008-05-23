6db783f0a0d28872cfaf928f2056fc55279aadf4
Uli Fouquet <uli@gnufix.de>
2008-05-23 11:52:33 +0000

    Move metadata zpts from www/ to browser/

--- src/psj/content/metadataregistry.py
+++ src/psj/content/metadataregistry.py
@@ -32,7 +32,7 @@ from zope.interface import implements
 
 from psj.content.interfaces import IMetadataSchemaRegistryTool
 
-_www = os.path.join(os.path.dirname(__file__), 'www')
+_www = os.path.join(os.path.dirname(__file__), 'browser')
 
 class MetadataSchemaRegistry(UniqueObject, ActionProviderBase, Folder):
     implements(IMetadataSchemaRegistryTool)
