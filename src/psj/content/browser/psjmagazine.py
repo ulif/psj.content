ea0c1c6d53329725b3552a18b462b5b79b76d8b2
Uli Fouquet <uli@gnufix.de>
2008-06-03 22:23:21 +0000

    Add view for psj magazines.

---
+++ src/psj/content/browser/psjmagazine.py
@@ -0,0 +1,33 @@
+##
+## psjmagazine.py
+## Login : <uli@pu.smp.net>
+## Started on  Wed Jun  4 00:23:53 2008 Uli Fouquet
+## $Id$
+## 
+## Copyright (C) 2008 Uli Fouquet
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+"""Views for PSJ magazines.
+"""
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class PSJMagazineView(BrowserView):
+    """Default view of a PSJ magazine.
+    """
+    __call__ = ViewPageTemplateFile('psjmagazine.pt')
+
+
+
