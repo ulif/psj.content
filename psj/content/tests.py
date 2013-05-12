0fa1c1a98dea51f380efbc0e7c39ddf6cc140479
ulif <uli@gnufix.fake>
2013-05-12 14:57:00 +0200

    Add single real test to start with preparing Travis CI.

--- psj/content/tests.py
+++ psj/content/tests.py
@@ -1,15 +1,22 @@
+# tests for psj.content
+
 import unittest
 
 #from zope.testing import doctestunit
 #from zope.component import testing
 from Testing import ZopeTestCase as ztc
 
-from Products.Five import fiveconfigure
+from plone.behavior.interfaces import IBehavior
+from plone.directives.form import IFormFieldProvider
+from Products.CMFDefault.Document import Document
+from Products.Five import fiveconfigure, zcml
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
+from zope.component import queryUtility
 ptc.setupPloneSite()
 
 import psj.content
+from psj.content.behaviors import IPSJAuthor
 
 
 class TestCase(ptc.PloneTestCase):
@@ -19,6 +26,7 @@ class TestCase(ptc.PloneTestCase):
         @classmethod
         def setUp(cls):
             fiveconfigure.debug_mode = True
+            #zcml.load_config('configure.zcml', psj.content)
             ztc.installPackage(psj.content)
             fiveconfigure.debug_mode = False
 
@@ -26,9 +34,22 @@ class TestCase(ptc.PloneTestCase):
         def tearDown(cls):
             pass
 
+class BehaviorAuthorTestCase(TestCase):
+
+    def test_author_installed(self):
+        # make sure we can get the 'Author' behavior after install
+        behavior = queryUtility(
+            IBehavior, name='psj.content.behaviors.IPSJAuthor',
+            default=None)
+        self.assertTrue(behavior is not None)
+        self.assertEqual(behavior.interface, IPSJAuthor)
+        # make sure the behavior provides IFormFieldProvider
+        self.assertEqual(
+            IFormFieldProvider.providedBy(behavior.interface), True)
+        return
 
 def test_suite():
-    return unittest.TestSuite([
+    suite = unittest.TestSuite([
 
         # Unit tests
         #doctestunit.DocFileSuite(
@@ -50,6 +71,9 @@ def test_suite():
         #    test_class=TestCase),
 
         ])
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(BehaviorAuthorTestCase))
+    return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
