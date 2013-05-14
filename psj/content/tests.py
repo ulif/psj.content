28a5fa37cd4f45f7dc094fdd1a169c121c42ec2a
ulif <uli@gnufix.fake>
2013-05-14 02:53:58 +0200

    Add real tests for PSJAuthor.

--- psj/content/tests.py
+++ psj/content/tests.py
@@ -6,13 +6,15 @@ import unittest
 #from zope.component import testing
 from Testing import ZopeTestCase as ztc
 
-from plone.behavior.interfaces import IBehavior
+from plone.behavior.interfaces import IBehavior, IBehaviorAssignable
+from plone.dexterity.interfaces import IDexterityContent
 from plone.directives.form import IFormFieldProvider
-from Products.CMFDefault.Document import Document
 from Products.Five import fiveconfigure, zcml
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
-from zope.component import queryUtility
+from zope.component import queryUtility, adapts, provideAdapter
+from zope.interface import implements
+from zope.schema.interfaces import WrongType
 ptc.setupPloneSite()
 
 import psj.content
@@ -34,6 +36,45 @@ class TestCase(ptc.PloneTestCase):
         def tearDown(cls):
             pass
 
+
+
+class DummyDocument(object):
+    # a dummy document that supports properties
+    implements(IDexterityContent)
+    portal_type = 'testtype'
+    def __init__(self, title):
+        self.title = title
+
+    def hasProperty(self, key):
+        return key in dir(self)
+
+    def setProperty(self, key, val):
+        setattr(self, key, val)
+
+    def getProperty(self, key):
+        return getattr(self, key)
+
+    def _updateProperty(self, key, val):
+        self.setProperty(key, val)
+
+class TestingAssignable(object):
+    # a IBehaviorAssignable adapter that adapts DummyContent objects
+    implements(IBehaviorAssignable)
+    adapts(DummyDocument)
+
+    enabled = [IPSJAuthor]
+
+    def __init__(self, context):
+        self.context = context
+
+    def supports(self, behavior_interface):
+        return behavior_interface in self.enabled
+
+    def enumerate_behaviors(self):
+        for e in self.enabled:
+            yield queryUtility(IBehavior, name=e.__identifier__)
+
+
 class BehaviorAuthorTestCase(TestCase):
 
     def test_author_installed(self):
@@ -47,6 +88,24 @@ class BehaviorAuthorTestCase(TestCase):
         self.assertEqual(
             IFormFieldProvider.providedBy(behavior.interface), True)
         return
+
+    def test_author_behavior_usable(self):
+        # we can get a behavior by adapter
+        doc = DummyDocument('doc')
+        provideAdapter(TestingAssignable)
+        self.assertEqual(IDexterityContent.providedBy(doc), True)
+        behavior = IPSJAuthor(doc, None)
+        self.assertTrue(behavior is not None)
+        self.assertEqual(True, hasattr(behavior, 'psj_author'))
+        # we can assign valid values to doc through the behavior
+        behavior.psj_author = u'John Cleese'
+        self.assertEqual(u'John Cleese', doc.psj_author)
+        # numbers are not accepted as TextLines
+        self.assertRaises(WrongType, setattr, behavior, 'psj_author', 1)
+        # also byte streams (non-unicode) are rejected
+        self.assertRaises(WrongType, setattr, behavior, 'psj_author', 'Cheese')
+        return
+
 
 def test_suite():
     suite = unittest.TestSuite([
