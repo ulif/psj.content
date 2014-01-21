67230e11723df3e3d718ab266cc7f7b3e8de20f4
ulif <uli@gnufix.fake>
2014-01-21 14:02:08 +0100

    New content type, basics.

---
+++ psj/content/psj_basedoc.py
@@ -0,0 +1,48 @@
+#  psj.content is copyright (c) 2014 Uli Fouquet
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#  MA 02111-1307 USA.
+#
+"""Dexterity base type for PSJ.
+
+"""
+from psj.content import _
+from five import grok
+from plone.dexterity.content import Container
+from plone.supermodel import model
+from zope import schema
+from zope.schema.fieldproperty import FieldProperty
+
+
+class IBaseDoc(model.Schema):
+    """A PSJ base.
+    """
+    psj_author = schema.List(
+        title=_(u'Autor'),
+        description=_(u'Autor(en) oder Herausgeber.'),
+        required=False,
+        readonly=True,
+        value_type=schema.TextLine(),
+        )
+
+
+class BaseDoc(Container):
+    """A PSJ document.
+    """
+    grok.implements(IBaseDoc)
+
+    __allow_access_to_unprotected_subobjects__ = 1
+
+    psj_author = FieldProperty(IBaseDoc["psj_author"])
