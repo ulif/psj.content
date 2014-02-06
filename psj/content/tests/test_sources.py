88de3aa7fefdb2fc0ee4908f2b377ebf72a613a7
ulif <uli@gnufix.fake>
2014-02-06 17:34:16 +0100

    Fix tests.

--- psj/content/tests/test_sources.py
+++ psj/content/tests/test_sources.py
@@ -7,7 +7,7 @@ from zope.schema.interfaces import (
     IContextSourceBinder, IVocabularyFactory,
     )
 from zope.schema.vocabulary import SimpleVocabulary
-from psj.content.sources import institutes_source
+from psj.content.sources import InstitutesSourceBinder, institutes_source
 from psj.content.testing import SampleVocabFactory
 
 
@@ -24,21 +24,21 @@ class SourcesUnitTests(unittest.TestCase):
     def tearDown(self):
         self._unregister()
 
-    def test_inst_src_iface(self):
-        src = institutes_source(None)
-        verify.verifyClass(IContextSourceBinder, institutes_source)
-        verify.verifyObject(IContextSourceBinder, src)
+    def test_inst_src_binder_iface(self):
+        binder = InstitutesSourceBinder()
+        verify.verifyClass(IContextSourceBinder, InstitutesSourceBinder)
+        verify.verifyObject(IContextSourceBinder, binder)
 
     def test_inst_src_wo_vocab(self):
-        src = institutes_source(None)
-        assert isinstance(src(None), SimpleVocabulary)
-        assert 'one' not in src(None)
+        src = institutes_source(context=None)
+        assert isinstance(src, SimpleVocabulary)
+        assert 'one' not in src
 
     def test_inst_src_w_vocab(self):
         factory = SampleVocabFactory()
         gsm = getGlobalSiteManager()
         gsm.registerUtility(factory, provided=IVocabularyFactory,
                             name=u'psj.content.Institutes')
-        src = institutes_source(None)(None)
+        src = institutes_source(context=None)
         assert isinstance(src, SimpleVocabulary)
         assert 'one' in src
