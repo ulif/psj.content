fbf24337659a9767ef9446ce9cd32094b291fb7a
ulif <uli@gnufix.fake>
2014-02-24 18:30:13 +0100

    Reflect new sources structure in basedoc tests.

--- psj/content/tests/test_psj_basedoc.py
+++ psj/content/tests/test_psj_basedoc.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # Tests for psj_basedoc module.
+import os
+import shutil
+import tempfile
 import unittest
+from base64 import b64encode
 from plone.app.testing import (
     TEST_USER_ID, SITE_OWNER_NAME, SITE_OWNER_PASSWORD, setRoles,
     )
@@ -11,10 +15,17 @@ from zope.event import notify
 from zope.interface import verify
 from zope.lifecycleevent import ObjectModifiedEvent
 from zope.schema.interfaces import IVocabularyFactory
+from psj.content.interfaces import IExternalVocabConfig
 from psj.content.psj_basedoc import (
     IBaseDoc, BaseDoc,
     )
 from psj.content.testing import INTEGRATION_TESTING, SampleVocabFactory
+
+
+VOCAB_ENTRIES = u'Vocab Entry 1\nVocab Entry 2\nÜmlaut Entry\n'
+VOCAB_TOKEN1 = b64encode('Vocab Entry 1')
+VOCAB_TOKEN2 = b64encode('Vocab Entry 2')
+VOCAB_TOKEN3 = b64encode('Ümlaut Entry')
 
 
 class BaseDocUnitTests(unittest.TestCase):
@@ -30,16 +41,41 @@ class BaseDocIntegrationTests(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
 
+    def create_external_vocab(self, name):
+        # create a working external vocab and register it
+        gsm = getGlobalSiteManager()
+        path = os.path.join(self.workdir, 'sample_vocab-%s.csv' % name)
+        open(path, 'w').write(
+            u'Vocab Entry 1\nVocab Entry 2\nÜmlaut Entry\n'.encode('utf-8'))
+        conf = {'path': path, 'name': name}
+        gsm.registerUtility(conf, provided=IExternalVocabConfig, name=name)
+
+    def setup_vocabs(self):
+        for name in [u'psj.content.Institutes', ]:
+            self.create_external_vocab(name)
+
+    def teardown_vocabs(self):
+        gsm = getGlobalSiteManager()
+        for name in [u'psj.content.Institutes', ]:
+            gsm.unregisterUtility(name=name, provided=IVocabularyFactory)
+
     def setUp(self):
+        self.workdir = tempfile.mkdtemp()
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.portal.invokeFactory('Folder', 'test-folder')
         setRoles(self.portal, TEST_USER_ID, ['Member'])
         self.folder = self.portal['test-folder']
+        self.setup_vocabs()
+        return
         vocab_factory = SampleVocabFactory()
         self.portal.getSiteManager().registerUtility(
             vocab_factory, provided=IVocabularyFactory,
             name=u'psj.content.Institutes')
+
+    def tearDown(self):
+        self.teardown_vocabs()
+        shutil.rmtree(self.workdir)
 
     def test_adding(self):
         # we can add BaseDoc instances
@@ -47,7 +83,7 @@ class BaseDocIntegrationTests(unittest.TestCase):
             'psj.content.basedoc', 'doc1',
             title=u'My Doc', description=u'My description.',
             psj_title=u'My Title', psj_subtitle=u'My Subtitle',
-            psj_institute='InstOne',
+            psj_institute=u'Vocab Entry 1',
             )
         d1 = self.folder['doc1']
         self.assertTrue(IBaseDoc.providedBy(d1))
@@ -56,7 +92,7 @@ class BaseDocIntegrationTests(unittest.TestCase):
         # additional attributes were set
         self.assertEqual(d1.psj_title, u'My Title')
         self.assertEqual(d1.psj_subtitle, u'My Subtitle')
-        self.assertEqual(d1.psj_institute, 'InstOne')
+        self.assertEqual(d1.psj_institute, u'Vocab Entry 1')
 
     def test_editing(self):
         # we can modify BaseDocs. Changes are reflected.
@@ -64,21 +100,21 @@ class BaseDocIntegrationTests(unittest.TestCase):
             'psj.content.basedoc', 'doc1',
             title=u'My doc', description=u'My description.',
             psj_title=u'My title', psj_subtitle=u'My Subtitle',
-            psj_institute=u'InstOne',
+            psj_institute=u'Vocab Entry 1',
             )
         d1 = self.folder['doc1']
         d1.title = u'My changed title'
         d1.description = u'My changed description'
         d1.psj_title = u'My changed title'
         d1.psj_subtitle = u'My changed subtitle'
-        d1.psj_institute = u'InstTwo'
+        d1.psj_institute = u'Vocab Entry 2'
         # we have to fire an event here
         notify(ObjectModifiedEvent(d1))
         self.assertEqual(d1.title, u'My changed title')
         self.assertEqual(d1.description, u'My changed description')
         self.assertEqual(d1.psj_title, u'My changed title')
         self.assertEqual(d1.psj_subtitle, u'My changed subtitle')
-        self.assertEqual(d1.psj_institute, u'InstTwo')
+        self.assertEqual(d1.psj_institute, u'Vocab Entry 2')
 
     def test_fti(self):
         # we can get factory type infos for base docs
@@ -167,12 +203,18 @@ class BasedocBrowserTests(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
 
-    def setup_vocabs(self):
-        vocab_factory = SampleVocabFactory()
+    def create_external_vocab(self, name):
+        # create a working external vocab and register it
         gsm = getGlobalSiteManager()
-        gsm.registerUtility(
-            vocab_factory, provided=IVocabularyFactory,
-            name=u'psj.content.Institutes')
+        path = os.path.join(self.workdir, 'sample_vocab-%s.csv' % name)
+        open(path, 'w').write(
+            u'Vocab Entry 1\nVocab Entry 2\nÜmlaut Entry\n'.encode('utf-8'))
+        conf = {'path': path, 'name': name}
+        gsm.registerUtility(conf, provided=IExternalVocabConfig, name=name)
+
+    def setup_vocabs(self):
+        for name in [u'psj.content.Institutes', ]:
+            self.create_external_vocab(name)
 
     def teardown_vocabs(self):
         gsm = getGlobalSiteManager()
@@ -184,10 +226,12 @@ class BasedocBrowserTests(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Member', 'Manager'])
         self.portal_url = self.portal.absolute_url()
         self.browser = Browser(self.layer['app'])
+        self.workdir = tempfile.mkdtemp()
         self.setup_vocabs()
 
     def tearDown(self):
         self.teardown_vocabs()
+        shutil.rmtree(self.workdir)
 
     def do_login(self, browser):
         browser.open(self.portal_url + '/login')
@@ -209,14 +253,14 @@ class BasedocBrowserTests(unittest.TestCase):
         self.browser.getControl(label='Summary').value = 'My Description'
         self.browser.getControl(label='Titel').value = 'My Book Title'
         self.browser.getControl(label='Untertitel').value = 'My Subtitle'
-        self.browser.getControl(label='Institut').value = ['InstOne', ]
+        self.browser.getControl(label='Institut').value = [VOCAB_TOKEN1, ]
         self.browser.getControl("Save").click()
 
         assert 'My Title' in self.browser.contents
         assert 'My Description' in self.browser.contents
         assert 'My Book Title' in self.browser.contents
         assert 'My Subtitle' in self.browser.contents
-        assert 'InstOne' in self.browser.contents
+        assert 'Vocab Entry 1' in self.browser.contents
 
         # edit tests follow here.
 
@@ -232,7 +276,7 @@ class BasedocBrowserTests(unittest.TestCase):
         self.browser.getControl(label='Summary').value = 'My Other Descr.'
         self.browser.getControl(label='Titel').value = 'Other Book Title'
         self.browser.getControl(label='Untertitel').value = 'Other Subtitle'
-        self.browser.getControl(label='Institut').value = ['InstTwo', ]
+        self.browser.getControl(label='Institut').value = [VOCAB_TOKEN2, ]
         self.browser.getControl("Save").click()
 
         assert 'Other Title' in self.browser.contents
@@ -243,5 +287,5 @@ class BasedocBrowserTests(unittest.TestCase):
         assert 'My Book Title' not in self.browser.contents
         assert 'Other Subtitle' in self.browser.contents
         assert 'My Subtitle' not in self.browser.contents
-        assert 'InstTwo' in self.browser.contents
-        assert 'InstOne' not in self.browser.contents
+        assert 'Vocab Entry 2' in self.browser.contents
+        assert 'Vocab Entry 1' not in self.browser.contents
