107ed8ff9fcf5eafc3baa75bcc324742b1d350ae
ulif <uli@gnufix.fake>
2014-03-26 15:26:37 +0100

    Provide customized catalog indexes.

---
+++ psj/content/setuphandlers.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#  psj.content is copyright (c) 2014 Uli Fouquet
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#  MA 02111-1307 USA.
+#
+"""
+Generic Setup stuff.
+
+"""
+import logging
+from Products.CMFCore.utils import getToolByName
+# The profile id of psj.content:
+PROFILE_ID = 'profile-psj.content:default'
+
+
+def add_catalog_indexes(context, logger=None):
+    """Method to add our wanted indexes to the portal_catalog.
+
+    When called from the import_various method below, 'context' is
+    the plone site and 'logger' is the portal_setup logger.  But
+    this method can also be used as upgrade step, in which case
+    'context' will be portal_setup and 'logger' will be None.
+    """
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('psj.content')
+
+    # Run the catalog.xml step as that may have defined new metadata
+    # columns.  We could instead add <depends name="catalog"/> to the
+    # registration of our import step in zcml, but doing it in code
+    # makes this method usable as upgrade step as well.  Note that
+    # this silently does nothing when there is no catalog.xml, so it
+    # is quite safe.
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'catalog')
+
+    catalog = getToolByName(context, 'portal_catalog')
+    indexes = catalog.indexes()
+    # Specify the indexes you want, with ('index_name', 'index_type')
+    wanted = (
+        # ('psj_author', 'FieldIndex'),
+        # ('psj_author', 'KeywordIndex'),
+        )
+    indexables = []
+    for name, meta_type in wanted:
+        if name not in indexes:
+            catalog.addIndex(name, meta_type)
+            indexables.append(name)
+            logger.info("Added %s for field %s.", meta_type, name)
+    if len(indexables) > 0:
+        logger.info("Indexing new indexes %s.", ', '.join(indexables))
+        catalog.manage_reindexIndex(ids=indexables)
+
+
+def import_various(context):
+    """Import step for configuration that is not handled in xml files.
+    """
+    # Only run step if a flag file is present
+    if context.readDataFile('psj.content-default.txt') is None:
+        return
+    logger = context.getLogger('psj.content')
+    site = context.getSite()
+    add_catalog_indexes(site, logger)
