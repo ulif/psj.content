2b68e57bbcfac1a0613578f3df15eb579ff5f368
ulif <uli@gnufix.de>
2014-05-07 18:33:49 +0200

    Add tests for new RedisKeysSource.

--- psj/content/tests/test_sources.py
+++ psj/content/tests/test_sources.py
@@ -9,12 +9,17 @@ from zope.schema.interfaces import (
     )
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from psj.content.sources import (
-    ExternalVocabBinder, RedisSource, make_terms, tokenize,
+    ExternalVocabBinder, RedisSource, RedisKeysSource, make_terms, tokenize,
     institutes_source, licenses_source, publishers_source,
     subjectgroup_source, ddcgeo_source, ddcsach_source, ddczeit_source,
     gndid_source,
     )
 from psj.content.testing import ExternalVocabSetup, RedisStoreSetup
+
+
+#: A test layer setting up a RedisServer in background.
+#: XXX: Move to`testing` module?
+RedisLayer = RedisStoreSetup(RedisServer)
 
 
 class TokenizeTests(unittest.TestCase):
@@ -78,7 +83,7 @@ class MakeTermsTests(unittest.TestCase):
 
 class RedisSourceTests(unittest.TestCase):
 
-    layer = RedisStoreSetup(RedisServer)
+    layer = RedisLayer
 
     def setUp(self):
         settings = self.layer.server.settings['redis_conf']
@@ -127,6 +132,53 @@ class RedisSourceTests(unittest.TestCase):
         self.assertEqual(term.token, 'Zm9v')
         self.assertTrue(hasattr(term, 'title'))
         self.assertEqual(term.title, u'bar')
+
+
+class RedisKeysSourceTests(unittest.TestCase):
+
+    layer = RedisLayer
+
+    def setUp(self):
+        settings = self.layer.server.settings['redis_conf']
+        port = settings['port']
+        self.redis = redis.StrictRedis(host='localhost', port=port, db=0)
+        self.redis.flushdb()
+        self.redis.set(u'foo', u'bar')
+        self.redis_host = settings['bind']
+        self.redis_port = settings['port']
+
+    def tearDown(self):
+        self.redis.flushdb()
+
+    def test_iface(self):
+        # make sure we fullfill promised interfaces
+        source = RedisKeysSource(host=self.redis_host, port=self.redis_port)
+        verify.verifyClass(IBaseVocabulary, RedisKeysSource)
+        verify.verifyObject(IBaseVocabulary, source)
+
+    def test_get_contained(self):
+        # we can tell whether a certain key is stored in redis store
+        source = RedisKeysSource(host=self.redis_host, port=self.redis_port)
+        result = u'foo' in source
+        self.assertEqual(result, True)
+
+    def test_get_uncontained(self):
+        # we can tell if a certain key is not in redis store
+        source = RedisKeysSource(host=self.redis_host, port=self.redis_port)
+        result = u'bar' in source
+        self.assertEqual(result, False)
+
+    def test_get_term_contained(self):
+        # we can get contained terms as ITerm
+        source = RedisKeysSource(host=self.redis_host, port=self.redis_port)
+        term = source.getTerm(u'foo')
+        assert ITitledTokenizedTerm.providedBy(term)
+        self.assertTrue(hasattr(term, 'value'))
+        self.assertEqual(term.value, u'foo')
+        self.assertTrue(hasattr(term, 'token'))
+        self.assertEqual(term.token, 'Zm9v')
+        self.assertTrue(hasattr(term, 'title'))
+        self.assertEqual(term.title, u'foo')
 
 
 class SourcesUnitTests(ExternalVocabSetup, unittest.TestCase):
