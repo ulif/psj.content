ce6182dd6cc5a78f294fa2f965df661a47fec956
ulif <uli@gnufix.de>
2015-07-29 17:48:39 +0200

    Update copyright year.

--- psj/content/behaviors.py
+++ psj/content/behaviors.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#  psj.content is copyright (c) 2014 Uli Fouquet
+#  psj.content is copyright (c) 2014, 2015 Uli Fouquet
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
