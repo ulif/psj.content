5fdb83eeb95236c380cc69485f32cdbf2976a2e8
ulif <uli@gnufix.de>
2015-10-24 18:47:24 +0200

    Give more progress hints when working.

--- scripts/normalize.py
+++ scripts/normalize.py
@@ -28,6 +28,7 @@ def normalize_list(inpath, outpath, separator):
     etc. Here the `separator` string is "&&" in both,
     input and output file.
     """
+    print("Opening %s for reading..." % inpath)
     with gzip.open(inpath, "r") as infile:
         with open(outpath, "w") as outfile:
             for cnt, line in enumerate(infile.readlines()):
@@ -39,6 +40,7 @@ def normalize_list(inpath, outpath, separator):
                 normalized = dinsort.normalize("%s(%s)" % (term, id_num))
                 out_term = "%s%s%s\n" % (normalized, separator, term)
                 outfile.write(out_term.encode("utf-8"))
+    print("Done. Written results to %s" % outpath)
 
 
 class TestNormalize(unittest.TestCase):
